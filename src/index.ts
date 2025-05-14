3b7e68dff5160724ff051bb96a3b3f5e28205ee9
IgorYudintsev <kakvospitat@gmail.com>
2025-05-14 13:27:00 +0300

    second commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 import express,{Request,Response} from 'express';
+import cors from "cors";
 const app = express();
 const port = 3000;
+
+
+// Включаем CORS, чтобы разрешить запросы с других доменов
+app.use(cors());
 
 app.get('/', (req:Request, res:Response) => {
     res.send('Hello TypeScript!');
