bfe71916a3abb9d3cbce45fa39b45de6d3181fd7
IgorYudintsev <kakvospitat@gmail.com>
2025-05-14 13:32:40 +0300

    third commit

--- dist/index.js
+++ dist/index.js
@@ -9,8 +9,11 @@ const app = (0, express_1.default)();
 const port = 3000;
 // Включаем CORS, чтобы разрешить запросы с других доменов
 app.use((0, cors_1.default)());
-app.get('/', (req, res) => {
-    res.send('Hello TypeScript!');
+// app.get('/', (req:Request, res:Response) => {
+//     res.send('Hello TypeScript!');
+// });
+app.get("/", (req, res) => {
+    res.json({ message: "Hello TypeScript!" }); // JSON, а не просто текст
 });
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,12 @@ const port = 3000;
 // Включаем CORS, чтобы разрешить запросы с других доменов
 app.use(cors());
 
-app.get('/', (req:Request, res:Response) => {
-    res.send('Hello TypeScript!');
+// app.get('/', (req:Request, res:Response) => {
+//     res.send('Hello TypeScript!');
+// });
+
+app.get("/", (req: Request, res: Response) => {
+    res.json({ message: "Hello TypeScript!" }); // JSON, а не просто текст
 });
 
 app.listen(port, () => {
