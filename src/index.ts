1f3b1410dcaadf2cc5f91a580fc8ef354e4384fc
IgorYudintsev <kakvospitat@gmail.com>
2025-05-15 15:12:59 +0300

    4 commit

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,21 @@ import express,{Request,Response} from 'express';
 import cors from "cors";
 const app = express();
 const port = 3000;
+app.use(cors());// Включаем CORS, чтобы разрешить запросы с других доменов
 
+const todos=[{title:'Express'},{title:'React'}]
+const books=[{volume:'Book1'},{volume:'Book2'}]
 
-// Включаем CORS, чтобы разрешить запросы с других доменов
-app.use(cors());
+app.get("/todos", (req: Request, res: Response) => {
+    res.send(todos);
+});
 
-// app.get('/', (req:Request, res:Response) => {
-//     res.send('Hello TypeScript!');
-// });
+app.get("/books", (req: Request, res: Response) => {
+    res.send(books);
+});
 
 app.get("/", (req: Request, res: Response) => {
-    res.json({ message: "Hello TypeScript!" }); // JSON, а не просто текст
+    res.json({ message: "Hello TypeScript!" });
 });
 
 app.listen(port, () => {
