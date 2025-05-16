9b979a371d3226d7e1cadf085643bdf49bcc510d
IgorYudintsev <kakvospitat@gmail.com>
2025-05-16 17:06:09 +0300

    14 commit

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const uuid_1 = require("uuid");
 const app = (0, express_1.default)();
 const port = 3000;
 app.use((0, cors_1.default)()); // Включаем CORS, чтобы разрешить запросы с других доменов
-const todos = [{ title: 'Express' }, { title: 'React' }];
+const todos = [
+    {
+        title: "What to learn",
+        filter: "all",
+        tasks: [
+            { taskId: (0, uuid_1.v1)(), title: "HTML&CSS", isDone: true },
+            { taskId: (0, uuid_1.v1)(), title: "JS", isDone: true }
+        ],
+    },
+    {
+        title: "What to do",
+        filter: "all",
+        tasks: [
+            { taskId: (0, uuid_1.v1)(), title: "HTML&CSS2", isDone: true },
+            { taskId: (0, uuid_1.v1)(), title: "JS2", isDone: true }
+        ],
+    }
+];
 const books = [{ volume: 'Book1' }, { volume: 'Book2' }];
 app.get("/todos", (req, res) => {
     res.send(todos);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,43 @@
 import express,{Request,Response} from 'express';
 import cors from "cors";
+import {v1} from "uuid";
 const app = express();
 const port = 3000;
 app.use(cors());// Включаем CORS, чтобы разрешить запросы с других доменов
 
-const todos=[{title:'Express'},{title:'React'}]
+type ObjectType = {
+    title: string
+    filter: FilterValuesType
+    tasks: Array<TasksType>
+  }
+export type TasksType = {
+    taskId: string
+    title: string
+    isDone: boolean
+}
+
+export type FilterValuesType = "all" | "active" | "completed";
+
+
+const todos:ObjectType[]=[
+    {
+            title: "What to learn",
+            filter: "all",
+            tasks: [
+                {taskId: v1(), title: "HTML&CSS", isDone: true},
+                {taskId: v1(), title: "JS", isDone: true}
+            ],
+        },
+        {
+            title: "What to do",
+            filter: "all",
+            tasks: [
+                {taskId: v1(), title: "HTML&CSS2", isDone: true},
+                {taskId: v1(), title: "JS2", isDone: true}
+            ],
+        }
+]
+
 const books=[{volume:'Book1'},{volume:'Book2'}]
 
 app.get("/todos", (req: Request, res: Response) => {
